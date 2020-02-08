82e83430d4d102b465555797df4245d8e629fb4a
Teo Mikelthun <teomiklethun@gmail.com>
2020-02-08 18:06:22 -0500

    Finished project (with bugs).

--- main.py
+++ main.py
@@ -4,8 +4,9 @@ import matplotlib.pyplot as plt
 import argparse
 
 
-def toHex(arr):
+def to_hex(arr):
     return arr[0] * 0x000001 + arr[1] * 0x000100 + arr[2] * 0x010000
+
 
 imArray = cv2.imread('laser.png')
 color_data = "{"
@@ -17,7 +18,7 @@ for frame in range(0, len(imArray), 8):
     frames += "{"
     for col in range(0, len(imArray[frame])):
         for row in range(frame, frame + 8):
-            hex_val = toHex(imArray[row][col])
+            hex_val = to_hex(imArray[row][col])
             if hex_val != 0:
                 colors.add(hex_val)
     for color in colors:
@@ -25,10 +26,10 @@ for frame in range(0, len(imArray), 8):
         frames += "{"
         color_data += hex(color) + ", "
         for col in range(0, len(imArray[frame])):
+            num_represent = 0
             for row in range(frame, frame + 8):
-                num_represent = 0
-                hex_color = toHex(imArray[row][col])
-                num_represent += (1 if hex_color == color else 0) * (2 ** (row - frame))
+                hex_color = to_hex(imArray[row][col])
+                num_represent += 2 ** (row - frame) if hex_color == color else 0
                 frames += hex(num_represent)
                 frames += ", "
         frames = frames[0:-2]
@@ -41,4 +42,21 @@ color_data = color_data[0:-2]
 frames = frames[0: -2]
 color_data += "}"
 frames += "}"
-print(frames)
+
+
+with open("code.txt", "w") as f:
+    print(f'''
+vector<vector<vector<uint8_t>>> frameData {frames}
+vector<vector<int>> colorData {color_data}
+
+frame %= frameData.size()
+
+vector<*ColorData> colorDataVector;
+for(size_t i = 0; i < colorData[frame].size(); ++i) {{
+    ColorData cd;
+    colorArr = uint8_t[32];
+    std::copy(frameData[frame][i].begin(), frameData[frame][i].end(), colorArr);
+    ColorData_init(cd, colorData[frame][i], colorArr);
+    display_color_data(cd);
+}}
+''', file=f)
